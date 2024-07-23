345fec348f56b6b175bc805a6f6c9604dcab8df1
Rushikesh Satkar <rushikesh.satkar@intelliswift.com>
2024-07-23 09:19:26 +0530

    Add project files.

---
+++ PracticeConsoleApp/OOP/OopProgram1.cs
@@ -0,0 +1,15 @@
+﻿namespace PracticeConsoleApp.OOP
+{
+    public abstract class AbstractClass
+    {
+        public abstract void AbstractPrintMethod();
+    }
+
+    public class Print : AbstractClass
+    {
+        public override void AbstractPrintMethod()
+        {
+            Console.WriteLine("Printing from Print class");
+        }
+    }
+}
---
+++ PracticeConsoleApp/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using PracticeConsoleApp;
+using PracticeConsoleApp.OOP;
+
+Print print = new Print();
+print.AbstractPrintMethod();
